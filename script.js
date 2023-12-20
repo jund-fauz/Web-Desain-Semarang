3aa99d3ecdfd8ac9199b38fa6f9abd762eb5e087
Mujadid Jundi Fauzi <mujadidjundifauzi200@gmail.com>
2023-12-20 10:31:35 +0700

    Third & Fourth Content

---
+++ script.js
@@ -0,0 +1,3 @@
+let fccHeight = document.getElementById('first-content-card').clientHeight
+const secondContent = document.getElementById('second-content')
+secondContent.style.marginTop = `${fccHeight / 2.3}px`
