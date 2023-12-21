2d0f06c27523f75e5e6e91458b15006182bfebc0
jund-fauz <mujadidjundifauzi200@gmail.com>
2023-12-21 17:01:32 +0700

    Responsive Tablet on First until Forth Content

--- script.js
+++
@@ -1,3 +0,0 @@
-let fccHeight = document.getElementById('first-content-card').clientHeight
-const secondContent = document.getElementById('second-content')
-secondContent.style.marginTop = `${fccHeight / 2.3}px`
